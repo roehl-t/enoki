99d9d150e64afca7d5e0b07f9d52b2e3fa643a43
roehl-t <98183032+roehl-t@users.noreply.github.com>
2022-03-29 12:05:55 -0400

    Update rnaseq_pipeline.config.sh
    added initial sequence processing programs
    will need to update list of samples to be the raw samples - pipeline can specify paired/unpaired naming conventions automatically

--- rnaseq_pipeline.config.sh
+++ rnaseq_pipeline.config.sh
@@ -13,17 +13,12 @@
 NUMCPUS=6
 
 #### Program paths ####
-
-## optional BINDIR, using it here because these programs are installed in a common directory
-#BINDIR=/usr/local/bin
-#HISAT2=$BINDIR/hisat2
-#STRINGTIE=$BINDIR/stringtie
-#SAMTOOLS=$BINDIR/samtools
-
 #if these programs are not in any PATH directories, please edit accordingly:
-#HISAT2=$(which hisat2)
-#STRINGTIE=$(which stringtie)
-#SAMTOOLS=$(which samtools)
+
+FASTQC=/Applications/FastQC.app/Contents/MacOS/fastqc
+FQTRIM=/Applications/fqtrim-0.9.7/fqtrim
+TRIMMOMATIC=/Applications/Trimmomatic-0.38/trimmomatic-0.38.jar
+INTERLEAVE=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/data/data_concat/fqtrim_output/interleave_pairs.py
 HISAT2=/Applications/hisat2-2.2.1/hisat2
 STRINGTIE=/Applications/stringtie-1.3.6.OSX_x86_64/stringtie
 SAMTOOLS=/Applications/samtools-1.8/samtools
@@ -39,6 +34,8 @@ HEATMAP=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/scripts/heatmap.R
 UNIPROTFASTA=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/scripts/map_UniProtKB_FASTA.py
 PANTHERSCORE=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/panther/pantherScore2.2/pantherScore2.2.pl
 PANTHERFPKM=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/scripts/map_panther_fpkm.R
+
+
 #### File paths for input data
 ### Full absolute paths are strongly recommended here.
 ## Warning: if using relatives paths here, these will be interpreted 
@@ -47,29 +44,24 @@ PANTHERFPKM=/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/scripts/map_pant
 ## parameter is provided to the main pipeline script)
 
 ## Optional base directory, if most of the input files have a common path
-# BASEDIR="/home/johnq/RNAseq_protocol/chrX_data"
-#BASEDIR=$(pwd -P)/HiSat2_analysis_Dpoly
 BASEDIR="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq"
+TEMPLOC="$BASEDIR/tmpdirec" #this will be relative to the output directory
+
+TRIMMOMATICADAPTERS="/Applications/Trimmomatic-0.38/adapters"
 FASTQLOC="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/data/data_qc_done"
-#FASTQLOC="$BASEDIR/samples"
 GENOMEIDX="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/genome_assemblies_all_files/ncbi-genomes-2021-11-01/GCA_011800155.1_ASM1180015v1/fvIndex"
-#GTFFILE="$BASEDIR/genes/Dpolymorpha_annotations.gtf"
 PHENODATA="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/fv_sampling_data.csv"
 BLASTDIR="/Applications/ncbi-blast-2.10.0+/bin"
 UNIPROTDIR="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/uniprot"
 RRNADIR="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/rRNA"
 PANTHERLIBDIR="/Volumes/RAID_5_data_arra/Todd/Thomas_Roehl_RNASeq/panther/target4/famlib/rel/PANTHER16.0_altVersion/ascii/PANTHER16.0" # download and extract any one of the .tgz files (all contain same data) from http://data.pantherdb.org/ftp/panther_library/current_release/
 
-TEMPLOC="$BASEDIR/tmpdirec" #this will be relative to the output directory
-
 ## list of samples 
 ## (only paired reads, must follow _1.*/_2.* file naming convention)
+############################## needs update -- these will be generated dynamically
 reads1=(${FASTQLOC}/*_fwd.fastq)
 reads1=("${reads1[@]##*/}")
 reads2=("${reads1[@]/_fwd/_rev}")
-#reads1=(${FASTQLOC}/*_R1_*)
-#reads1=("${reads1[@]##*/}")
-#reads2=("${reads1[@]/_R1_/_R2_}")
 unpaired1=("${reads1[@]/_pairs_fwd/_unpaired}")
 unpaired2=(${FASTQLOC}/*_1U.fqtrimmed.fq)
 unpaired2=("${unpaired2[@]##*/}")
