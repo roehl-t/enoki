33c865de74ce1c722b8a67f35c479eb23352d1f8
roehl-t <98183032+roehl-t@users.noreply.github.com>
2022-06-09 15:43:50 -0400

    Update rnaseq_pipeline.config.sh

--- rnaseq_pipeline.config.sh
+++ rnaseq_pipeline.config.sh
@@ -12,7 +12,7 @@
 #### User Options ####
 
 # location for all log files
-LOGLOC=
+LOGLOC="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/working/logs"
 
 # data directory -- location of the .fastq.gz files. All data files should be in a single directory. Please begin with the .fastq.gz file format or edit rnaseq_pipeline.sh to match the one you have.
 DATADIR=
@@ -29,12 +29,12 @@ USEUNPAIRED=Y
 
 # working directory -- where to put files that are being worked on currently
   # hint: use an SSD for speed improvement
-WRKDIR=
+WRKDIR="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/working"
 
 # final directory -- where the files should go for storage
   # hint: use a large drive
   ## must have a different name than the working directory
-DESTDIR=
+DESTDIR="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/run_2022"
 
 # how many CPUs to use on the current machine?
 NUMCPUS=6
@@ -104,28 +104,42 @@ UNIPROTFILE="/Volumes/RAID_5_data_array/Todd/Thomas_Roehl_RNASeq/uniprot/uniprot
   # to automatically remove samples containing below a certain threshold number of sequences, add "auto-####" to SETNAMES
   # to run the analysis on all samples, add "auto-all" to SETNAMES
   # do not add a removelist for "auto-###" or "auto-all" setnames -- we suggest putting all auto-##### setnames at the end of the SETNAMES list to be sure your manual removelists are in the correct order
-SETNAMES=("auto-all" "300" "5k" "culnor" "priyou")
-removelist2="-51 -50 -48 -45 -39 -21"
-removelist3="-12 -14 -17 -21 -22 -29 -30 -31 -33 -34 -38 -39 -40 -42 -45 -48 -50 -51 -52 -53 -56 -57"
+SETNAMES=("auto-all" "auto-300" "auto-5000" "culnor" "priyou")
+#################removelist2="-51 -50 -48 -45 -39 -21"
+#################removelist3="-12 -14 -17 -21 -22 -29 -30 -31 -33 -34 -38 -39 -40 -42 -45 -48 -50 -51 -52 -53 -56 -57"
 removelist4="-10 -11 -13 -18-B -21 -25 -27 -28 -30 -31 -32 -34 -38 -39 -43 -44 -45 -48 -51 -52 -54-B -55 -56 -57"
 removelist5="-51 -48 -39 -12 -14 -15 -16 -17 -19 -20 -22 -23 -24 -26 -29 -33 -35 -36 -37 -40 -41 -42 -46 -47 -49 -50 -53"
-REMOVELISTS=("${removelist2}" "${removelist3}" "${removelist4}" "${removelist5}")
+#################REMOVELISTS=("${removelist2}" "${removelist3}" "${removelist4}" "${removelist5}")
+REMOVELISTS=("${removelist4}" "${removelist5}")
   # to remove a sample from every set, add it here
     # some files may be too small for StringTie to handle, so list them here as you encounter the errors (although, rnaseq_pipeline.sh should automatically find and remove them for you)
-REMOVEALWAYS="-51 -48 -39"
+##################REMOVEALWAYS="-51 -48 -39"
+REMOVEALWAYS=""
 
 
-#### Other Options
 
-# BUSCO dataset name -- what taxonomic group should be used as the BUSCO dataset (see instructions BUSCO's website)?
+#### BUSCO options
+
+# what version of BUSCO should be run?
+## check https://hub.docker.com/r/ezlabgva/busco/tags?page=1&ordering=last_updated for latest version "Tag" number
+BUSCOVERSION="v5.3.2_cv1"
+# what taxonomic group should be used as the BUSCO dataset
+## see instructions on https://busco.ezlab.org/
 BUSCODATASET="agaricales_odb10"
+# a short name for the Docker container running BUSCO
+## the tag will be appended to "busco_output_" to generate the Docker container name
+## the docker container name must be one that has never been used before on your machine, so choose a tag that is unique to this dataset
+BUSCOTAG="mojo"
 
-# Ballgown options
-    # to work with more than one set, specify the covariates and confounding variables separately for each set
-    # make sure you specify an equal number of setnames and covariates/confounding variables
-    # only one covariate should be specified for each set
-    # mutliple confounding variables may be specified for each set
-  # column name in the phenotype data file to be used as the covariate by Ballgown
+
+
+#### Ballgown options
+
+# to work with more than one set, specify the covariates and confounding variables separately for each set
+# make sure you specify an equal number of setnames and covariates/confounding variables
+# only one covariate should be specified for each set
+# mutliple confounding variables may be specified for each set
+# column name in the phenotype data file to be used as the covariate by Ballgown
 covset1="tissue"
 covset2="tissue"
 covset3="tissue"
@@ -140,23 +154,52 @@ adjvars4=("tissue")
 adjvars5=("tissue")
 ADJVARSETS=("${adjvars1}" "${adjvars2}" "${adjvars3}" "${adjvars4}" "${adjvars5}")
   # for PCA, specify pairs (or singles) of comparisons to make
-pcapair1=("batch" "jar")
-pcapair2=("type" "tissue")
-pcapair3=("position")
+pcapair1="jar batch"
+pcapair2="type tissue"
+pcapair3="position"
 PCAPAIRS="${pcapair1};${pcapair2};${pcapair3}"
+
+
+
+#### Other Options
 
 # NCBI API key (if none, type "")
 NCBIAPI=""
 
 # for creating heatmaps and bar plots
   # these lists need to be separated by commas
-  # will partial match to the gene, organism, or sample name
+  # will partial match to the gene, organism, or sample
 genelist1="cytochrome"
 genelist2="hydrophobin,psh"
 genelist3="mitogen,mapk,map"
 genelist4="elongation"
-MAPGENELISTS=("${genelist1}" "${genelist2}" "${genelist3}" "${genelist4}")
+genelist5="heat,hsp"
+MAPGENELISTS=("${genelist1}" "${genelist2}" "${genelist3}" "${genelist4}" "${genelist5}")
 orglist1="Flammulina"
 MAPORGLISTS=("${orglist1}")
-samplelist1=("nor,cul")
+# sample matching is done by pheno data column
+  # use this format: "column_name_1,condition_1,condition_2;column_name_2,condition_3"
+  # conditions for each column should be grouped together and separated by commas
+    # the first word should be the name of the pheno data colum, followed by a comma
+    # next, list all the conditions that should be matched (text only, partial matches allowed)
+  # the conditions for one column should be separated from conditions for the next column by a semicolon
+  # to keep a sample, it must match at least one condition specified for each column
+samplelist1="type,nor,cul"
 MAPSAMPLELISTS=("${samplelist1}")
+# to label samples with their entry from a pheno data column, write the column names separated by commas below
+  # to not use any labels, type "none"
+ADDTONAMES="type,tissue"
+# to make data labels smaller when labeling with pheno data, you can truncate the pheno data entries to the number of letters you define below
+  # to keep the full name, input 0
+SHORTENNAMES=3
+
+# subset lists to write while matching PANTHER ids to FPKM values
+  # to match samples, use this format: "column_name_1,condition_1,condition_2;column_name_2,condition_3|column_name_1,condition_4"
+  # conditions for each column should be grouped together and separated by commas
+    # the first word should be the name of the pheno data colum, followed by a comma
+    # next, list all the conditions that should be matched (text only, partial matches allowed)
+  # the conditions for one column should be separated from conditions for the next column by a semicolon
+  # to keep a sample, it must match at least one condition specified for each column
+  # to create multiple subsets, separate each subset with a pipe (|)
+PANTHERSUBSETS="ids,ROEHL|tissue,myc|tissue,sti|tissue,pil|tissue,gil|type,you|type,pri|type,cul|type,nor"
+
